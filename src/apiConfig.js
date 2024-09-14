a6e9fb00c44c57ae646aded36f872aa0f12a18a8
Jacob Clapper <105309528+bostonbachexchange@users.noreply.github.com>
2024-09-13 23:02:53 -0400

    first commit

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
     // YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
-	// production: 'https://elias-dagher-api.onrender.com',
+	// production: 'https://robby-brown-api.onrender.com',
 	production: 'https://elias-api.fly.dev',
 	development: 'http://localhost:8000',
 }
